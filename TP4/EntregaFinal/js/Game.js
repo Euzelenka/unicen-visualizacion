bf422f6cb943b3c1555f186045a095f52d38ddea
Eugenia Zelenka <eugeniazelenka@gmail.com>
2017-10-25 23:17:50 -0300

    enemy

--- TP4/EntregaFinal/js/Game.js
+++ TP4/EntregaFinal/js/Game.js
@@ -6,7 +6,7 @@ class Game {
     this.points = 0;
     this.player = p;
     this.enemies = [];
-    this.apple = [];
+    this.cantApple = 5;
     self = this;
   }
 
@@ -27,14 +27,45 @@ class Game {
     document.getElementById('floor').style.animationPlayState = 'paused';
   }
 
-  appleUpdate() {
-    
+  appleUpdate(px) {
+    for (var i = 1; i <= this.cantApple; i++) {
+           let apple =  $("#apple"+i);
+           if(parseInt(apple.offset().left)>-100){
+               apple.offset({left: parseInt(apple.offset().left)-px});
+           }else{
+               let distanceX = Math.floor((Math.random() * 2000) + innerWidth);
+               let distanceY = Math.floor((Math.random() * 490) + 250);
+               this.updateDistance("apple"+i,distanceX,distanceY);
+           }
+       }
+  }
+
+  enemyUpdate(px) {
+    let enemy =  $("#enemy");
+       if(parseInt(enemy.offset().left)>-100){
+           enemy.offset({left: parseInt(enemy.offset().left)-px});
+       }else{
+           let distanceX = Math.floor((Math.random() * 2000) + innerWidth);
+           this.updateDistance("enemy",distanceX);
+       }
+  }
+
+  updateDistance(object,distanceX,distanceY){
+      let div = document.getElementById(object);
+      if(object == 'enemy'){
+          div.style.left = parseInt(div.style.left,10) + distanceX+'px';
+      }else{
+          div.style.left = parseInt(div.style.left,10) + distanceX+'px';
+          div.style.top = parseInt(div.style.top,10) + distanceY+'px';
+      }
   }
 
   update() {
     if(this.walk == true) {
       this.player.walk();
+      this.appleUpdate(2);
       this.moveBackground();
+      this.enemyUpdate();
     }
   }
 }
